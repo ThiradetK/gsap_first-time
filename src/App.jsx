e32e9052233b7cb019563c3d8e95f8cbbed486e1
ThiradetK <thiradet.k28@gmail.com>
2025-10-14 20:40:33 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex-center h-[100dvh]">
+      <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
+    </div>
+  );
+};
+
+export default App;
